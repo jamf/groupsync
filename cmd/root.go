e1edb72291f7b238d7b689a182aa315241cd442d
Tomasz Kurcz <tomasz.kurcz@jamf.com>
2019-10-14 14:46:48 +0200

    HC-1784 Skeleton

---
+++ cmd/root.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "groupsync",
+	Short: "Groupsync is a tool for syncing LDAP groups with GitHub teams.",
+	Long:  `Groupsync is a tool for syncing LDAP groups with GitHub teams.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"stash.jamf.build/devops/groupsync/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}

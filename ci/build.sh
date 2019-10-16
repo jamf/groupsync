7d2786c883ceba37c0ce82800c093de3a44a39ad
Tomasz Kurcz <tomasz.kurcz@jamf.com>
2019-10-16 11:47:04 +0200

    Versioning improvements

--- ci/build.sh
+++ ci/build.sh
@@ -1,1 +1,5 @@
-go build -i -v -ldflags="-X stash.jamf.build/devops/groupsync/cmd.version=$(git describe --always --dirty)"
+#!/usr/bin/env bash
+
+# Build the project, injectingthe version variable from git describe output
+
+go build -i -v -ldflags="-X stash.jamf.build/devops/groupsync/cmd.version=$(git describe --always --dirty)" stash.jamf.build/devops/groupsync

276573d994bd12b4d944c695db05ca4fae8bf316
Tomasz Kurcz <tomasz.kurcz@jamf.com>
2019-10-16 11:59:00 +0200

    typo

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-# Build the project, injectingthe version variable from git describe output
+# Build the project, injecting the version variable from git describe output
 
 go build -i -v -ldflags="-X stash.jamf.build/devops/groupsync/cmd.version=$(git describe --always --dirty)" stash.jamf.build/devops/groupsync

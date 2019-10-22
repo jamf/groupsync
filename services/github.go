7ef054a9247e502bca3da547fcc84c82e57927e5
Tomasz Kurcz <tomasz.kurcz@jamf.com>
2019-10-22 15:04:47 +0200

    HC-1807 Half-baked GitHub queries

--- services/github.go
+++ services/github.go
@@ -1,11 +1,21 @@
 package services
 
+import (
+	"context"
+	"fmt"
+
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+)
+
 type GitHub struct {
-	cfg GitHubConfig
+	client *githubv4.Client
+	cfg    GitHubConfig
 }
 
 type GitHubConfig struct {
 	Token string
+	Org   string
 }
 
 func NewGitHub() *GitHub {
@@ -15,5 +25,56 @@ func NewGitHub() *GitHub {
 }
 
 func (g *GitHub) GroupMembers(group string) ([]User, error) {
+	g.initClient()
+
+	var membersQuery struct {
+		Viewer struct {
+			Organization struct {
+				Team struct {
+					Name    string
+					Members struct {
+						Edges []struct {
+							Node struct {
+								ID   string
+								Name string
+							}
+						}
+					}
+				} `graphql:"team(slug: $grp)"`
+			} `graphql:"organization(login: $org)"`
+		}
+	}
+
+	vars := map[string]interface{}{
+		"org": githubv4.String(g.cfg.Org),
+		"grp": githubv4.String(group),
+	}
+
+	err := g.client.Query(
+		context.Background(),
+		&membersQuery,
+		vars,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	if membersQuery.Viewer.Organization.Team.Name == "" {
+		return nil, fmt.Errorf("Cannot find GitHub team called \"%s\"", group)
+	}
+
+	fmt.Println(membersQuery.Viewer.Organization.Team)
+
 	return nil, nil
+}
+
+func (g *GitHub) initClient() {
+	if g.client == nil {
+		src := oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: g.cfg.Token},
+		)
+		httpClient := oauth2.NewClient(context.Background(), src)
+
+		g.client = githubv4.NewClient(httpClient)
+	}
 }

7b0e91607e141fc5e88ca299d93254eb7c9d4be0
Tomasz Kurcz <tomasz.kurcz@jamf.com>
2019-11-18 14:12:50 +0100

    HC-1827 Change `sync` subcommand syntax

--- cmd/sync.go
+++ cmd/sync.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"strings"
 
@@ -29,12 +30,31 @@ type groupIdent struct {
 	svc   string
 }
 type mapping struct {
-	src groupIdent
+	src []groupIdent
 	tar groupIdent
 }
 
+func newMapping(src []groupIdent, tar groupIdent) mapping {
+	return mapping{
+		src: src,
+		tar: tar,
+	}
+}
+
+func (m mapping) diff() ([]services.User, []services.User) {
+	var flattenedSrc []services.User
+
+	for _, src := range m.src {
+		for _, user := range src.group {
+			flattenedSrc = append(flattenedSrc, user)
+		}
+	}
+
+	return services.Diff(flattenedSrc, m.tar.group, m.tar.svc)
+}
+
 var syncCmd = &cobra.Command{
-	Use:   "sync <source> <target>",
+	Use:   "sync <source>... <target>",
 	Args:  cobra.MinimumNArgs(2),
 	Short: "List the members of a group (or groups)",
 	Long:  `List the members of a group (or groups).`,
@@ -44,68 +64,69 @@ var syncCmd = &cobra.Command{
 			panic("sync is not implemented yet except with the dry-run flag")
 		}
 
-		mappings, err := parseMappings(args)
+		mapping, err := parseCLIMapping(args)
 		if err != nil {
 			panic(err)
 		}
 
-		for _, mapping := range mappings {
-			rem, add := services.Diff(
-				mapping.src.group,
-				mapping.tar.group,
-				mapping.tar.svc,
-			)
-			fmt.Printf(
-				"Results for %s:%s -> %s:%s:\n",
-				Cyan(mapping.src.svc),
-				Blue(mapping.src.name),
-				Cyan(mapping.tar.svc),
-				Blue(mapping.tar.name),
-			)
-			fmt.Printf(
-				"Rem: %+v\nAdd: %+v\n\n",
-				rem,
-				add,
+		rem, add := mapping.diff()
+
+		var b bytes.Buffer
+
+		b.WriteString("Sources:\n")
+		for _, src := range mapping.src {
+			b.WriteString(
+				fmt.Sprintf(
+					"- %s:%s\n",
+					Cyan(src.svc),
+					Blue(src.name),
+				),
 			)
 		}
+
+		b.WriteString("Target:\n")
+		b.WriteString(
+			fmt.Sprintf(
+				"- %s:%s\n",
+				Cyan(mapping.tar.svc),
+				Blue(mapping.tar.name),
+			),
+		)
+
+		b.WriteString("Rem:\n")
+		for _, u := range rem {
+			b.WriteString(
+				fmt.Sprintf("- %v\n", u.String()),
+			)
+		}
+
+		b.WriteString("Add:\n")
+		for _, u := range add {
+			b.WriteString(
+				fmt.Sprintf("- %v\n", u.String()),
+			)
+		}
+
+		fmt.Println(b.String())
 	},
 }
 
-func parseMappings(args []string) ([]mapping, error) {
-	if len(args)%2 != 0 {
-		return nil, fmt.Errorf("uneven number of arguments")
-	}
-
-	var result []mapping
-
-	for i := 0; i < len(args); i += 2 {
-		mapping, err := parseMapping(args[i], args[i+1])
+func parseCLIMapping(args []string) (mapping, error) {
+	var sources []groupIdent
+	for _, srcString := range args[:len(args)-1] {
+		src, err := parseGroupIdent(srcString)
 		if err != nil {
-			return nil, err
+			return mapping{}, err
 		}
-
-		result = append(result, mapping)
+		sources = append(sources, src)
 	}
 
-	return result, nil
-}
-
-func parseMapping(ident1, ident2 string) (mapping, error) {
-	src, err := parseGroupIdent(ident1)
-	if err != nil {
-		return mapping{}, err
-	}
-	tar, err := parseGroupIdent(ident2)
+	target, err := parseGroupIdent(args[len(args)-1])
 	if err != nil {
 		return mapping{}, err
 	}
 
-	result := mapping{
-		src: src,
-		tar: tar,
-	}
-
-	return result, nil
+	return newMapping(sources, target), nil
 }
 
 func parseGroupIdent(str string) (groupIdent, error) {
